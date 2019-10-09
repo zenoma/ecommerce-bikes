07c24d014b28322c0c0d76d19494151ce9dda474
Borja Padín Gómez <borja2padin@gmail.com>
2019-10-09 20:23:45 +0200

    Definir atributos clases principales

--- ws-app-model/src/main/java/es/udc/ws/bikes/model/bike/Bike.java
+++ ws-app-model/src/main/java/es/udc/ws/bikes/model/bike/Bike.java
@@ -1,5 +1,35 @@
 package es.udc.ws.bikes.model.bike;
 
+import java.util.Date;
+
 public class Bike {
-	// TODO Atributos, getters, setters, hash, equals
+	//Attributes - Pueden faltar
+	String modelName;
+	String description;
+	Date startDate;
+	float price;
+	int availableNumber;
+	Date adquisitionDate;//Fecha de alta del modelo - nombre puede variar
+	//getters
+	public String getModelName() {
+		return modelName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public int getAvailableNumber() {
+		return availableNumber;
+	}
+	public Date getAdquisitionDate() {
+		return adquisitionDate;
+	}
+	//Los setters no se deberían generar ya que podrían concurrir en modificaciones de información indeseada
+	//La información debería ser inicializada con métodos constructores
+	// TODO constructores, hash, equals
 }
--- ws-app-model/src/main/java/es/udc/ws/bikes/model/rent/Rent.java
+++ ws-app-model/src/main/java/es/udc/ws/bikes/model/rent/Rent.java
@@ -1,5 +1,29 @@
 package es.udc.ws.bikes.model.rent;
 
+import java.util.Date;
+
 public class Rent {
-	// TODO Atributos, getters, setters, hash, equals
+	//Attributes - Pueden faltar
+	String usserEmail;
+	Integer creditCard;//puede que necesite otro tipo por la longitud del código de als tarjetas
+	Date startRentDate;
+	Date finishRentDate;
+	int rentNumber;
+	//getters
+	public String getUsserEmail() {
+		return usserEmail;
+	}
+	public Integer getCreditCard() {
+		return creditCard;
+	}
+	public Date getStartRentDate() {
+		return startRentDate;
+	}
+	public Date getFinishRentDate() {
+		return finishRentDate;
+	}
+	public int getRentNumber() {
+		return rentNumber;
+	}
+	// TODO setters, hash, equals
 }
