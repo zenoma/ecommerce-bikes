811f5ad18bd3034b14e792bf9bbc0f4a05747ec3
Borja Padín Gómez <borja2padin@gmail.com>
2019-12-04 21:03:35 +0100

    Implementar doGet Bikes

--- ws-app-service/src/main/java/es/udc/ws/app/restservice/servlets/BikesServlet.java
+++ ws-app-service/src/main/java/es/udc/ws/app/restservice/servlets/BikesServlet.java
@@ -1,6 +1,7 @@
 package es.udc.ws.app.restservice.servlets;
 
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -15,6 +16,7 @@ import es.udc.ws.app.serviceutil.BikeToBikeDtoConversor;
 import es.udc.ws.bikes.model.bike.Bike;
 import es.udc.ws.bikes.model.bikeservice.BikeServiceFactory;
 import es.udc.ws.util.exceptions.InputValidationException;
+import es.udc.ws.util.exceptions.InstanceNotFoundException;
 import es.udc.ws.util.servlet.ServletUtils;
 
 @SuppressWarnings("serial")
@@ -45,17 +47,43 @@ public class BikesServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// TODO Bike Get
+		Long bikeId;
+		Bike bike;
 		String path = ServletUtils.normalizePath(req.getPathInfo());
-		if (path == null || path.length() == 0) {
+		if (path != null && (req.getParameter("keywords")!=null) && (req.getParameter("dateAvailable")!=null)) {
 			// FIXME Añadir fechas OBLIGATORIAS
 			String keywords = req.getParameter("keywords");
+			Calendar dateAvailable = null;
+			dateAvailable = getDate(req.getParameter("dateAvailable"));
 			List<Bike> bikes = BikeServiceFactory.getService()
-					.findBikes(keywords, null);
+					.findBikes(keywords, dateAvailable);
 			List<ServiceBikeDto> bikesDto = BikeToBikeDtoConversor
 					.toBikeDtos(bikes);
 			ServletUtils.writeServiceResponse(resp, HttpServletResponse.SC_OK,
 					JsonServiceBikeDtoConversor.toArrayNode(bikesDto), null);
-		} else {
+		} else if (path != null && (req.getParameter("id")!=null)){
+			String bikeIdString = req.getParameter("id");
+			try {
+				bikeId = Long.valueOf(bikeIdString);
+			}catch (NumberFormatException ex) {
+				ServletUtils.writeServiceResponse(resp,
+						HttpServletResponse.SC_BAD_REQUEST,
+						JsonServiceExceptionConversor.toInputValidationException(
+								new InputValidationException("Invalid bike Id: " +path)),
+						null);
+				return;
+			}
+			try {
+				bike = BikeServiceFactory.getService().findBike(bikeId);
+			}catch(InstanceNotFoundException ex){
+				ServletUtils.writeServiceResponse(resp,
+						HttpServletResponse.SC_NOT_FOUND,
+						JsonServiceExceptionConversor.toInputValidationException(
+								new InputValidationException("Bike Id not found: " +path)),
+						null);
+				return;
+			}
+		}else {
 			ServletUtils.writeServiceResponse(resp,
 					HttpServletResponse.SC_BAD_REQUEST,
 					JsonServiceExceptionConversor.toInputValidationException(
@@ -63,6 +91,21 @@ public class BikesServlet extends HttpServlet {
 					null);
 		}
 		super.doGet(req, resp);
+	}
+	
+	private static Calendar getDate(String date) {
+		String[] dateSplit = date.split("-");
+		
+		int day = Integer.valueOf(dateSplit[0]);
+		int month = Integer.valueOf(dateSplit[1]);
+		int year = Integer.valueOf(dateSplit[2]);
+		
+		Calendar dateAvailableAux = Calendar.getInstance();
+		dateAvailableAux.set(Calendar.DAY_OF_MONTH,day);
+		dateAvailableAux.set(Calendar.MONTH,month);
+		dateAvailableAux.set(Calendar.YEAR,year);
+		
+		return dateAvailableAux;
 	}
 
 }
